547ddc720ddedef773d8f8cbf5e19043075ef52e
Alejandro <alejandrog96@outlook.es>
2023-02-23 00:07:46 -0300

    Creando InputDialog Opciones

--- ChallengeBackEnd/src/Vista/Program.java
+++ ChallengeBackEnd/src/Vista/Program.java
@@ -1,10 +1,16 @@
 package Vista;
 
+import Modelo.Option;
+import Modelo.OptionType;
+import javax.swing.JOptionPane;
+
 public class Program {
     
     public static void main(String[] args) {
         
-        
-        
+        Object[] opciones = {new Option("Conversor de monedas", OptionType.C_Monedas), new Option("Conversor de temperatura", OptionType.C_Temperatura)};
+
+        Option e = (Option) JOptionPane.showInputDialog(null, "Elija una opción", "input", JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+
     }
 }
